8c35389497e4b29a905cef9cd51a67fb3edb6fea
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-04-29 19:18:51 +0200

    constant value

--- ES6/CONST/app.js
+++ ES6/CONST/app.js
@@ -1,4 +1,17 @@
-/* const/
+/* const*/
+
+const ARR = [1, 3, 7];
+
+// ARR = [3, 4, 6]; What do you think will happen? Why?
+//ARR.push(11); and here?
+
+console.log(ARR);
+
+/* We don't change the constant value, we are only changing the 
+values which the constant is pointing to.
+Revise By Value and By Reference lecture: 
+https://github.com/nicolasleivab/JavaScript-Under-the-Hood/tree/master/By_Value_vs_By_Reference
+*/
 
 
 
