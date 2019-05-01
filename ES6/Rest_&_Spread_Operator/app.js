3f1da8ef35a47b6104dc75cc40164dbbee2fa025
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-05-01 05:03:17 +0200

    Spread Operator

--- ES6/Rest_&_Spread_Operator/app.js
+++ ES6/Rest_&_Spread_Operator/app.js
@@ -15,3 +15,14 @@ Rest operator simply takes a set of values and pack them
 in an array to pass it as a set arguments to the function
 */
 
+
+/* Spread Operator*/
+
+let sample2 = [2, 5, 10, 33];
+
+//console.log(...sample2);
+console.log(Math.max(...sample2));
+
+/*
+Spread is use to split up an array in to a list of values (the opposite)
+*/
