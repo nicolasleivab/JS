1e82b223e817efeb59a55cc45ebd247cf2d1d951
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-07-25 18:10:21 +0200

    Values_and_Coercion

--- scripts/1.Program Structure/Values_and_Coercion.js
+++ scripts/1.Program Structure/Values_and_Coercion.js
@@ -1,3 +1,29 @@
+/* Types of Values */
+
+/* Types of values can be goruped by:
+-Primitives: Number, string, boolean (true or false), null (empty value), undefined (empty value)
+-Complex: Arrays, objects and functions
+
+Primitives can be accessed by their value while complex types can be accessed by reference (explained in the Objects chapter)
+*/
+/* Corecion */
+
+let sum = 3 + 3*'2' + '11';
+
+console.log(sum); // Before trying this in the console, guess the answer: a) 23, b) 20 , c) 911, d) 119 or e) 33211
+
+//JavaScript will prioritize concatenation (+) when using a number and a string (converting number to string)
+//In the case of a multiplication (*) JS will interprete it as a math operation in any case (converting string to number)
+
+
+let sum2 = 2 + true; //true and false are coerced to 1 and 0 respectively
+console.log (sum2);
+
+let sum3 = 3 + undefined; //What will happen in this case?, what if you change the number type for a string in the two last sums ('2' and '3')
+console.log(sum3);        //Try this with the null value as well
+
+
+
 /* Default Values*/
 
 function greet(name){
@@ -15,9 +41,4 @@ function greet(name){
 	name = name || "<Your name here>"   //this way you set a default value
 	console.log("Hello " + name);
 }
-
-/* Now imagine default values causing conflicts between 
-different libraries that you are loading in your app
-
-check: https://blog.acolyer.org/2018/06/20/conflictjs-finding-and-understanding-conflicts-between-javascript-libraries/
-
+*/
